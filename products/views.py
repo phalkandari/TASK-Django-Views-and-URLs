f447610075d7dbb571040eda03535c590bf3f0eb
phalkandari <mohammad.kandary@gmail.com>
2022-10-12 18:39:27 +0300

    Finished 1st part: Home View

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
+def get_home(request):
+    return HttpResponse("<h1> Welcome to this Drinks Website!!<h1/>")
--- productshop/urls.py
+++ productshop/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+from products.views import get_home
+
+urlpatterns = [path("admin/", admin.site.urls), path("home/", get_home)]
